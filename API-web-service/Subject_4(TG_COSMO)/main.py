ae537a1de5f0b885b66f85ec5ac0eef1e41c8700
MaksimObozniy <mobozn@gmail.com>
2024-12-04 02:34:52 +0400

    Make file path to download images

--- API-web-service/Subject_4(TG_COSMO)/main.py
+++ API-web-service/Subject_4(TG_COSMO)/main.py
@@ -1,10 +1,16 @@
 import requests
+import os
 
-filename = "hubble.jpeg"
+
+file_name = "hubble.jpeg"
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 
+folder_name = "images"
+os.makedirs(folder_name, exist_ok=True)
+
+file_path = os.path.join(folder_name, file_name)
 response = requests.get(url)
 response.raise_for_status()
 
-with open(filename, 'wb') as file:
+with open(file_path, 'wb') as file:
     file.write(response.content)
